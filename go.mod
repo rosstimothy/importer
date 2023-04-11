0508cad106c131f444151e1becc7c51d9fac9be9
Tim Ross <tim.ross@goteleport.com>
2023-04-11 17:52:35 -0400

    parse and export traces

---
+++ go.mod
@@ -0,0 +1,29 @@
+module github.com/rosstimothy/importer
+
+go 1.18
+
+require (
+	github.com/golang/protobuf v1.5.2
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.8.0
+	go.opentelemetry.io/proto/otlp v0.18.0
+	google.golang.org/grpc v1.49.0
+)
+
+require (
+	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.0 // indirect
+	go.opentelemetry.io/otel v1.8.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.8.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.8.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.8.0 // indirect
+	go.opentelemetry.io/otel/trace v1.8.0 // indirect
+	golang.org/x/net v0.0.0-20220809184613-07c6da5e1ced // indirect
+	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20220808204814-fd01256a5276 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"time"
+
+	"github.com/golang/protobuf/jsonpb"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	tracepb "go.opentelemetry.io/proto/otlp/trace/v1"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	var traces = flag.String("dir", "", "Path to traces")
+	flag.Parse()
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Kill, os.Interrupt)
+	defer cancel()
+
+	client := otlptracegrpc.NewClient(
+		otlptracegrpc.WithDialOption(grpc.WithBlock()),
+		otlptracegrpc.WithInsecure(),
+	)
+
+	dialCtx, dialCancel := context.WithTimeout(ctx, 5*time.Second)
+	defer dialCancel()
+
+	if err := client.Start(dialCtx); err != nil {
+		log.Fatalf("failed to start client: %v", err)
+	}
+	defer client.Stop(context.Background())
+
+	var spans []*tracepb.ResourceSpans
+	if err := filepath.WalkDir(*traces, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() {
+			return nil
+		}
+
+		f, err := os.Open(path)
+		if err != nil {
+			return nil
+		}
+		defer f.Close()
+
+		log.Printf("Exporting trace %q", path)
+
+		scanner := bufio.NewScanner(f)
+		scanner.Buffer(make([]byte, 1024*1024), 1024*1024*1024)
+		scanner.Split(bufio.ScanLines)
+
+		for scanner.Scan() {
+			var span tracepb.ResourceSpans
+			if err := jsonpb.UnmarshalString(scanner.Text(), &span); err != nil {
+				return nil
+			}
+			spans = append(spans, &span)
+
+		}
+		if err := scanner.Err(); err != nil {
+			return fmt.Errorf("problem reading trace file: %w", err)
+		}
+
+		return nil
+	}); err != nil {
+		log.Fatalf("failed to parse traces in %q: %v", *traces, err)
+	}
+
+	if err := client.UploadTraces(ctx, spans); err != nil {
+		log.Fatalf("failed to upload traces: %v", err)
+	}
+
+	if err := client.Stop(context.Background()); err != nil {
+		log.Fatalf("failed to stop client: %v", err)
+	}
+}
